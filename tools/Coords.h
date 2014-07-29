97be520bd9dbceaa8745a28e77da87ccb6b315d1
Charles Ofria <charles.ofria@gmail.com>
2014-07-29 00:48:05 -0400

    Minor updates to Coords class.

--- tools/Coords.h
+++ tools/Coords.h
@@ -17,12 +17,18 @@ namespace emk {
     inline int GetX() const { return x; }
     inline int GetY() const { return y; }
 
+    Coords & Set(int _x, int _y) { x=_x; y=_y; return *this; }
+
     double Magnitude() const { return sqrt( (double) (x * x + y * y) ); }
 
     Coords operator+(const Coords & _in) const { return Coords(GetX() + _in.GetX() , GetY() + _in.GetY()); }
     Coords operator-(const Coords & _in) const { return Coords(GetX() - _in.GetX() , GetY() - _in.GetY()); }
     Coords & operator+=(const Coords & _in) { x += _in.GetX(); y += _in.GetY(); return *this; }
     Coords & operator-=(const Coords & _in) { x -= _in.GetX(); y -= _in.GetY(); return *this; }
+
+    Coords & Rot90() { return Set(y, -x); }
+    Coords & Rot180() { return Set(-x, -y); }
+    Coords & Rot270() { return Set(-y, x); }
   };
 
 };
