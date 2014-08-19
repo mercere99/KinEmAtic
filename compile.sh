99a954a022d564f87d27fa3ab5d99a2947a4e6bc
Jason Stredwick <jason.stredwick@gmail.com>
2014-08-19 11:52:23 -0400

    Updated the compile script to be more generic and allow the specification of the output directory.  Also, moving html along with the JavaScript.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,19 @@
 #!/bin/tcsh
-python /Users/charles/Local/emsdk_portable/emscripten/1.16.0/emcc -std=c++11 --js-library libs/library_kinetic.js -g4 -Werror -DEMK_DEBUG $1.cc -o $1.js -s EXPORTED_FUNCTIONS="['_emkMain', '_emkJSDoCallback']"
-cp $1.js /Users/charles/Sites/EmCanvas/
+
+set COMMAND = emcc
+set INPUT = ""
+$COMMAND -v >& /dev/null
+if ($status != "0") then
+  set COMMAND = python
+  set INPUT = /Users/charles/Local/emsdk_portable/emscripten/1.16.0/emcc
+endif
+
+if ($2 == "") then
+  set OUTDIR = /Users/charles/Sites/EmCanvas/
+else
+  set OUTDIR = $2
+endif
+
+$COMMAND $INPUT -std=c++11 --js-library libs/library_kinetic.js -g4 -Werror -DEMK_DEBUG $1.cc -o $1.js -s EXPORTED_FUNCTIONS="['_emkMain', '_emkJSDoCallback']"
+cp $1.js $OUTDIR
+cp $1.html $OUTDIR
